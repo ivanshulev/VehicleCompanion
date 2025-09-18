f05c5bb12cc0c7e6669efde11238aa2fdbdd2d33
Ivan Shulev <ivanshulevbg@yandex.com>
2025-09-18 15:29:41 +0300

    Initial Commit

---
+++ VehicleCompanion/VehicleCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VehicleCompanionApp.swift
+//  VehicleCompanion
+//
+//  Created by Ivan Shulev on 18.09.25.
+//
+
+import SwiftUI
+
+@main
+struct VehicleCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
